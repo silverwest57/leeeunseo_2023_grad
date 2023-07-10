46e1483b5a60eeb203d25f38ee26a9d45343e3af
silverwest57 <101112147+silverwest57@users.noreply.github.com>
2023-07-11 05:16:49 +0900

    손우성 교수님 업로드 실습함

---
+++ src/subject2/script/reference.js
@@ -0,0 +1,31 @@
+const reference = [
+  {
+    author: "lena polishko",
+    title: "",
+    year: "2023",
+    sourceTitle: "",
+    link: "https://unsplash.com/@ilon_a",
+  },
+  {
+    author: "ash thorp",
+    title: "M E C H A - YouTube",
+    year: "2021",
+    sourceTitle: "",
+    link: "https://vimeo.com/486708402",
+  },
+  {
+    author: "ash thorp",
+    title: "FITC Tokyo 2015 Titles",
+    year: "2015",
+    sourceTitle: "",
+    link: "https://vimeo.com/118919656",
+  },
+  {
+    author: "웃소 Wootso",
+    title:
+      "날달걀 먹고 삶은달걀 먹은척하기!! ㅋㅋㅋㅋ 웃소 최고의 거짓말쟁이는?",
+    year: "2019",
+    sourceTitle: "",
+    link: "https://www.youtube.com/watch?v=Oil6tNEMro8&ab_channel=%EC%9B%83%EC%86%8CWootso",
+  },
+];
